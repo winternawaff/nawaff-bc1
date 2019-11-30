b94d7b13022b4c8089eedba342f703d9d2daa1dd
winternawaff <57063193+winternawaff@users.noreply.github.com>
2019-11-30 13:29:47 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: '',    
+    YT_API_KEY: 'AIzaSyDeoIH0u1e72AtFpwSKKOSy3IPp2UHzqi4',
+    prefix: '',
+    devs: ['470673278709530634']
+}
---
+++ server.js
@@ -0,0 +1,100 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://project.glitch.me/`);
+}, 280000);
+ 
+const { Client, RichEmbed } = require("discord.js");
+var { Util } = require('discord.js');
+const {TOKEN, YT_API_KEY, prefix, devs} = require('./config')
+const client = new Client({ disableEveryone: true})
+const ytdl = require("ytdl-core");
+const canvas = require("canvas");
+const Canvas = require("canvas");
+const convert = require("hh-mm-ss")
+const fetchVideoInfo = require("youtube-info");
+const botversion = require('./package.json').version;
+const simpleytapi = require('simple-youtube-api')
+const moment = require("moment");
+const fs = require('fs');
+const util = require("util")
+const gif = require("gif-search");
+const opus = require("node-opus");
+const ms = require("ms");
+const jimp = require("jimp");
+const { get } = require('snekfetch');
+const guild = require('guild');
+const dateFormat = require('dateformat');//npm i dateformat
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube('AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8');
+const hastebins = require('hastebin-gen');
+const getYoutubeID = require('get-youtube-id');
+const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
+const pretty = require("pretty-ms");
+client.login(TOKEN);
+const queue = new Map();
+var table = require('table').table
+const Discord = require('discord.js');
+
+
+client.on('ready', () => {
+    console.log(`
+    # | Name : ${client.user.tag}
+    # | Servers : ${client.guilds.size}
+    # | Users : ${client.users.size}
+    By : US`);
+    });
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+client.on('message', message => {//for dev
+  var argresult = message.content.split(` `).slice(1).join(' ');
+
+if (message.content.startsWith(prefix + 'setgame')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else
+  if (message.content.startsWith(prefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(prefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else
+if (message.content.startsWith(prefix + 'setT')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/faresgameryt");
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+}
+
+client.on('message', message => {//restart
+    if(message.content === prefix + "restart") {
+              message.channel.send(`⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**`);
+            console.log(`⚠️ جاري اعادة تشغيل البوت... ⚠️`);
+            client.destroy();
+            child_process.fork(__dirname + "/server.js");
+            console.log(`تم اعادة تشغيل البوت`);
+        }
+
+
+    });
+});
+
